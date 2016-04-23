c1b0248ac9e0bca895375289b60ed8357d19a0b8
potix <poti.dog@gmail.com>
2016-04-24 04:51:54 +0900

    support airborne-drone

---
+++ platforms/airborne-drone/client/service/gatt.go
@@ -0,0 +1,25 @@
+package service
+
+import (
+	"fmt"
+	"github.com/potix/gatt"
+)
+
+var (
+	attrGATTUUID   = gatt.UUID16(0x1801)
+	attrDeviceName = gatt.UUID16(0x2A00)
+)
+
+func NewGattService() *gatt.Service {
+	s := gatt.NewService(attrGATTUUID)
+	s.AddCharacteristic(attrDeviceName).HandleReadFunc(
+		func(rsp gatt.ResponseWriter, req *gatt.ReadRequest) {
+			v := []byte{ 'g', 'o', 'b', 'o', 't' }
+			fmt.Println("$ Device Name Read Handler")
+			_, error := rsp.Write(v)
+			if error != nil {
+				fmt.Println(error)
+			}
+		})
+	return s
+}
